717a92d22d69e788a1bf59f371cfdf446bfdaf6b
notedit <notedit@gmail.com>
2022-01-08 19:22:21 +0800

    learn

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
+
+    testscope();    
+}
+
+
+
+fn testscope() {
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
 }
